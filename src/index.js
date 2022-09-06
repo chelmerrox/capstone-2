679a1a34ef354a11a41a8af12b568215d3669293
Losalini <c.rokocakau@gmail.com>
2022-09-06 21:16:48 +1200

    Create getAllMeals() function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import { response } from 'express';
 import './style.css';
+
+const mealsContainer = document.querySelector('.meals-container');
+
+const getAllMeals = async () => {
+  const options = {
+    method: 'GET',
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+  };
+
+  await fetch('https://www.themealdb.com/api/json/v1/1/categories.php', options)
+    .then((response) => response.json())
+    .then((data) => displayMeals(data.categories));
+};
