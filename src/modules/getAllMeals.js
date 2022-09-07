33a8ff2b67d50a9b4f3282ca41de7ee7da482941
Losalini <c.rokocakau@gmail.com>
2022-09-08 05:37:07 +1200

    Add popup window

--- src/modules/getAllMeals.js
+++ src/modules/getAllMeals.js
@@ -1,5 +1,9 @@
 const mealsContainer = document.querySelector('.meals-container');
-const modalContainer = document.querySelector('.modal-container');
+//let dataModalTarget = [];
+let k = 0;
+let overlay;
+const dataModalTarget = ['modal-1', 'modal-2', 'modal-3', 'modal-4', 'modal-5', 'modal-6', 'modal-7',
+'modal-8', 'modal-9', 'modal-10', 'modal-11', 'modal-12', 'modal-13', 'modal-14'];
 
 const displayMeals = (data) => {
   const grid = document.createElement('div');
@@ -20,13 +24,63 @@ const displayMeals = (data) => {
       <p class="likes-text"><span class="num-of-likes">5</span>likes</p>
       
       <div class="comment-and-reservations-container">
-        <button type="button" class="comments-btn">Comments</button>
+        <button type="button" class="comments-btn modal-${i + 1}" data-modal-target="modal-${mealData.idCategory}">Comments</button>
+
+        <!--Popup window container for Comments Button-->
+        <div class="modal popup-${i + 1}" id="modal-${i + 1}">
+          <div class="modal-header">
+            <span class="close-button close-button-${i + 1} data-close-button"><i>X</i></span>
+          </div>
+
+          <div class="modal-body">
+            <img src="${mealData.strCategoryThumb}" />
+            <h2>${mealData.strCategory}</h2>
+            <p class="description">${mealData.strCategoryDescription}</p>
+            <div>
+              <h3 class="comments">Comments</h3>
+              <ul class="comments-container">
+                <li class="comment"></li>
+              </ul>
+            </div>
+            <form>
+              <input type="text" placeholder="Your Name" />
+              <textarea placeholder="Your comment"></textarea>
+              <input type="button" value="Comment" />
+            </form>
+          </div>
+        </div>
+
         <button type="button" class="reservations-btn">Reservations</button>
       </div>
     `;
 
     grid.appendChild(mealContainer);
     mealsContainer.appendChild(grid);
+  });
+
+  dataModalTarget.forEach((id) => {
+    console.log(`id: ${id}`);
+  
+    // the open a Modal button
+    const commentBtn1 = document.querySelector(`[data-modal-target='${id}']`);
+    console.log(commentBtn1);
+
+    k += 1; // for the close-button-number class
+    const closeModalButtons = document.querySelector(`.close-button-${k}`); // the close a Modal button
+
+    // To open modal button
+    commentBtn1.addEventListener('click', () => {
+      const modal = document.getElementById(`${id}`);
+      openModal(modal);
+    });
+
+    overlay = document.getElementById('overlay');
+
+    // To close a modal by clicking on the close button
+    closeModalButtons.addEventListener('click', () => {
+      const modal = closeModalButtons.closest('.modal');
+      closeModal(modal);
+    });
   });
 };
 
@@ -41,38 +95,16 @@ const getAllMeals = async () => {
     .then((data) => displayMeals(data.categories));
 };
 
+function openModal(modal) {
+  if (modal === null) { return; }
+  modal.classList.add('active');
+  overlay.classList.add('active');
+};
+
+function closeModal(modal) {
+  if (modal === null) { return; }
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+};
+
 export default getAllMeals;
-
-const displayModal = (img, category, desc) => {
-  const commentsModal = document.createElement('div');
-  commentsModal.classList.add('comments-modal');
-  commentsModal.innerHTML = `
-  <span><i>X</i></span>
-  <img src="${img}" />
-  <h2>${category}</h2>
-  <p class="description">${desc}</p>
-  <div>
-    <h3 class="comments">Comments</h3>
-    <ul class="comments-container">
-      <li class="comment"></li>
-    </ul>
-  </div>
-  <form>
-    <input type="text" placeholder="Your Name" />
-    <textarea placeholder="Your comment"></textarea>
-    <input type="button" value="Comment" />
-  </form>`;
-  modalContainer.appendChild(commentsModal);
-};
-
-export const displayModalData = () => {
-  getAllMeals().then((mealData) =>
-    displayModal(
-      mealData.strCategoryThumb,
-      mealData.strCategory,
-      mealData.strCategoryDescription
-    )
-  );
-};
-
-displayModal();
