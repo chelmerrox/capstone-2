1e64b77492ff6c8ff386e82633d6777acdfa345b
unknown <shyusupova4@gmail.com>
2022-09-07 23:35:34 +0500

    Modify styles

--- src/modules/getAllMeals.js
+++ src/modules/getAllMeals.js
@@ -2,8 +2,22 @@ const mealsContainer = document.querySelector('.meals-container');
 //let dataModalTarget = [];
 let k = 0;
 let overlay;
-const dataModalTarget = ['modal-1', 'modal-2', 'modal-3', 'modal-4', 'modal-5', 'modal-6', 'modal-7',
-'modal-8', 'modal-9', 'modal-10', 'modal-11', 'modal-12', 'modal-13', 'modal-14'];
+const dataModalTarget = [
+  'modal-1',
+  'modal-2',
+  'modal-3',
+  'modal-4',
+  'modal-5',
+  'modal-6',
+  'modal-7',
+  'modal-8',
+  'modal-9',
+  'modal-10',
+  'modal-11',
+  'modal-12',
+  'modal-13',
+  'modal-14',
+];
 
 const displayMeals = (data) => {
   const grid = document.createElement('div');
@@ -17,19 +31,21 @@ const displayMeals = (data) => {
       <img src="${mealData.strCategoryThumb}" alt="Meal ${i + 1}"/>
 
       <div class="name-and-like-icon-container">
-        <h3>${mealData.strCategory}</h3>
+        <h3 class="dish-name">${mealData.strCategory}</h3>
         <i class="material-icons">favorite_border</i>
       </div>
       
       <p class="likes-text"><span class="num-of-likes">5</span>likes</p>
       
       <div class="comment-and-reservations-container">
-        <button type="button" class="comments-btn modal-${i + 1}" data-modal-target="modal-${mealData.idCategory}">Comments</button>
+        <button type="button" class="comments-btn modal-${i + 1}"
+        data-modal-target="modal-${mealData.idCategory}">Comments</button>
 
         <!--Popup window container for Comments Button-->
         <div class="modal popup-${i + 1}" id="modal-${i + 1}">
           <div class="modal-header">
-            <span class="close-button close-button-${i + 1} data-close-button"><i>X</i></span>
+            <span class="close-button close-button-${i + 1}
+            data-close-button"><i>X</i></span>
           </div>
 
           <div class="modal-body">
@@ -37,20 +53,15 @@ const displayMeals = (data) => {
             <h2>${mealData.strCategory}</h2>
             <p class="description">${mealData.strCategoryDescription}</p>
             <div>
-              <h3 class="comments">Comments</h3>
-              <ul class="comments-container">
-                <li class="comment"></li>
-              </ul>
+              <br><h3 class="comments">Comments</h3>
             </div>
-            <form>
-              <input type="text" placeholder="Your Name" />
-              <textarea placeholder="Your comment"></textarea>
-              <input type="button" value="Comment" />
+            <form class="comments-form">
+              <input class="form-input" type="text" placeholder="Your Name" />
+              <textarea class="form-input" placeholder="Your Comment"></textarea>
+              <input class="comment-btn" type="button" value="Comment" />
             </form>
           </div>
         </div>
-
-        <button type="button" class="reservations-btn">Reservations</button>
       </div>
     `;
 
@@ -60,7 +71,7 @@ const displayMeals = (data) => {
 
   dataModalTarget.forEach((id) => {
     console.log(`id: ${id}`);
-  
+
     // the open a Modal button
     const commentBtn1 = document.querySelector(`[data-modal-target='${id}']`);
     console.log(commentBtn1);
@@ -96,15 +107,19 @@ const getAllMeals = async () => {
 };
 
 function openModal(modal) {
-  if (modal === null) { return; }
+  if (modal === null) {
+    return;
+  }
   modal.classList.add('active');
   overlay.classList.add('active');
-};
+}
 
 function closeModal(modal) {
-  if (modal === null) { return; }
+  if (modal === null) {
+    return;
+  }
   modal.classList.remove('active');
   overlay.classList.remove('active');
-};
+}
 
 export default getAllMeals;
